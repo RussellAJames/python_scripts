75f39ae9e7059349e5de3ec9d33c3a0b69d22839
RussellAJames <rj17bv@brocku.ca>
2021-07-18 11:20:11 -0400

    Updating

--- 3070_stock_check.py
+++ 3070_stock_check.py
@@ -5,7 +5,6 @@ import time
 
 #### Checking Stock of individual store locations of GTX 3070 and GTX 3070 TI
 
-
 store_dict = {
 'online / main warehouse' : "https://www.canadacomputers.com/index.php?sort=3b&cPath=43_557_559&sf=:3_6,3_7&loc=PWH&mfr=&pr=",
 'burlington' : "https://www.canadacomputers.com/index.php?sort=3b&cPath=43_557_559&sf=:3_6,3_7&loc=BURL&mfr=&pr=",
@@ -26,20 +25,26 @@ list = []
 
 print('Current Stock of Canada Computers for GTX 3070 and 3070ti ...' + "\n" + "-------------------------------")
 
+### Checking each store for stock
 
 for i in store_dict:
     raw_data = urlopen(store_dict[i])
     unparsed = raw_data.read()
     gpu_soup = soup(unparsed, 'html.parser')
-    
-    dict = {}
-    htmlstring = gpu_soup.select('#product-list > div > div  div.col-12.productImageDesc span.text-dark')
-    price = gpu_soup.select('#product-list   div div  div.col-12.productImageDesc  div  span.d-block.mb-0')
-    pattern1 = r"href=(.+?)\>"
-    pattern2 = r"\>([A-Z].+?)\</a></span>"
-    pattern3 = r"\<strong\>(.+?)\</strong\>"
+
+
+### Using CSS selectors to return each GPU details, and GPU price details from HTML
+
+    htmlstring = gpu_soup.select('#product-list > div > div  div.col-12.productImageDesc span.text-dark') 
+    price = gpu_soup.select('#product-list   div div  div.col-12.productImageDesc  div  span.d-block.mb-0') 
+
+    pattern1 = r"href=(.+?)\>"                      ### Pulling out GPU URL from HTML
+    pattern2 = r"\>([A-Z].+?)\</a></span>"          ### Pulling out GPU Name from HTML
+    pattern3 = r"\<strong\>(.+?)\</strong\>"        ### Pulling out GPU Price from HTML
+
     price = re.findall(pattern3,str(price))
     store_name = str(i).title()
+    dict = {}
     gpu_name = []
     gpu_price = []
     gpu_url = []
@@ -51,7 +56,7 @@ for i in store_dict:
         gpu_price.append(str(price[t]))
         gpu_url.append(str(re.findall(pattern1,str(htmlstring))[t]))
 
-        dict[re.findall(pattern2, str(htmlstring))[t]] =    "Price: " + str(price[t]) +  "   URL:" +  str(re.findall(pattern1,str(htmlstring))[t])
+        
 
 
     if len(gpu_name) == 0:
@@ -59,10 +64,13 @@ for i in store_dict:
         list.append(store_name)
 
 
+### Returning location of stocked stores and each graphics card details as soon as found
+
     else: 
         for t in gpu_name:
             for g in gpu_price:
                 for h in gpu_url:
+                    
                      print(f"In Stock at Store Location: {store_name} " + "\n" + str(t) +  "\n" + "Price: " + str(g) + "\n" + "URL: " + str(h) + "\n" + "-------------------------------")
 
 string = ""
@@ -72,6 +80,8 @@ for i in list:
     else:    
         string = string + i + ", "
 
+
+### Returning location of out of stock stores at the end
 
 print("Store Locations " + string + " are Out of Stock")
 
